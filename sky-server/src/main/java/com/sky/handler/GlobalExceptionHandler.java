8230f16338590335b78ed49ef7a4dd2d955f6a4c
陈松 <2945075520@qq.com>
2023-10-31 14:54:43 +0800

    新增员工业务代码开发

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.sql.SQLNonTransientException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -22,6 +26,22 @@ public class GlobalExceptionHandler {
     public Result exceptionHandler(BaseException ex){
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
+    }
+
+    /**
+     * 处理sql异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        log.error("异常信息：{}", ex.getMessage());
+        String message = ex.getMessage();
+        if (message.contains("Duplicate entry")){
+            return Result.error(message.split(" ")[2]+ MessageConstant.ALREADY_EXISTS);
+        }else {
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
     }
 
 }
