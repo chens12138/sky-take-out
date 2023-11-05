eb678fb2a4235a9b4c2a179831827264a4aa0695
陈松 <2945075520@qq.com>
2023-11-05 16:34:22 +0800

    删除菜品业务代码开发

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,10 +1,17 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
+
+import java.util.List;
 
 public interface DishService {
 
 
     void addWithFlavor(DishDTO dishDTO);
 
+    PageResult page(DishPageQueryDTO dishPageQueryDTO);
+
+    void deleteBatch(List<Long> ids);
 }
