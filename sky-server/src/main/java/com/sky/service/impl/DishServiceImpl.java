7ea68dc2ec773546f5b0b4e3db426e6e4d8a13b6
陈松 <2945075520@qq.com>
2023-11-05 16:49:50 +0800

    删除菜品业务代码开发优化

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -72,11 +72,14 @@ public class DishServiceImpl implements DishService {
         if (setmealIdsByDishIds!=null && setmealIdsByDishIds.size()>0){
             throw new DeletionNotAllowedException(MessageConstant.DISH_BE_RELATED_BY_SETMEAL);
         }
-        for (Long id : ids) {
-            dishMapper.deleteById(id);
-            dishFlavorMapper.deleteByDishId(id);
-        }
-
+//        for (Long id : ids) {
+//            dishMapper.deleteById(id);
+//            dishFlavorMapper.deleteByDishId(id);
+//        }
+        //根据菜品ID集合批量删除菜品数据
+        dishMapper.deleteByIds(ids);
+        //根据菜品ID集合批量删除关联的口味数据
+        dishFlavorMapper.deleteByDishIds(ids);
     }
 
 }
